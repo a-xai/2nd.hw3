5c9e928295ef39563a9520645f7f9d5df892c1ca
a-xai <akylaib007@gmail.com>
2021-02-06 11:29:42 +0300

    Corrected some code.

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -3,28 +3,21 @@ package com.company;
 public class BankAccount {
     private double amount;
 
-    public int setAmount(double amount) {
-        this.amount = amount;
-        return 0;
-    }
-
     public double getAmount() {
         return amount;
     }
 
-    public double deposit(double sum) {
+    public double deposit(int sum) {
         return amount = sum;
     }
 
-    public int withdraw(int sum) throws LimitException {
+    public void withdraw(int sum) throws LimitException {
         if (amount > sum) {
-            setAmount(getAmount() - sum);
+            amount = amount - sum;
         } else {
-            throw new LimitException("У вас недостаточно денег на счету. ", amount);
-
+            throw new LimitException("У вас недостаточно денег на счету. \nСчитано с карты: " + amount, amount);
         }
 
-        return (int) getAmount();
     }
 }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,24 +4,27 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LimitException {
         BankAccount bankAccount = new BankAccount();
-        System.out.println("Сколько денег хотите снять ?");
-        Scanner scanner = new Scanner(System.in);int m = scanner.nextInt();
         bankAccount.deposit(20000);
+        System.out.println("На вашем счете: " + bankAccount.getAmount() + " coм.");
+        System.out.println("Сколько денег хотите снять ?");
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt();
+
+
         while (true) {
             try {
                 System.out.println(bankAccount.getAmount());
                 bankAccount.withdraw(m);
             } catch (LimitException e) {
                 System.out.println(e.getMessage());
+                System.out.println(bankAccount.getAmount() - bankAccount.getAmount());
 
-                bankAccount.setAmount(bankAccount.getAmount() - bankAccount.getAmount());
-                System.out.println(bankAccount.getAmount());
-            }
+                bankAccount.withdraw((int) bankAccount.getAmount());
+            } finally {
+                if (bankAccount.getAmount() == 0) {
 
-            finally {
-                if(bankAccount.getAmount()==0){
                     break;
                 }
             }
